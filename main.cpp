67a21dc2dff10fcc84e98b896b70ba5db0a3be21
Jarimorajro <183761369+Jarimorajro@users.noreply.github.com>
2024-10-27 23:56:48 +0100

    Testy z grafiką.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include "raylib.h"
 
+Texture2D tlo_epizod1 = LoadTexture("Grafika1/Tlo1.png");
+
 int main() {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(1200, 600, "Labirynty i Quizy");
+    char okno = 'l';
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawTexture(tlo_epizod1, 0.0, 0.0, WHITE);
+        if (IsTextureReady(tlo_epizod1)) DrawText("TAK", 100.0, 100.0, 25, BLACK);
+        // DrawTexturePro(tlo_epizod1, { 0.0f, 0.0f, 1920.0f, 1080.0f }, { 0.0f, 0.0f, 1200.0f, 600.0f }, { 0.0f, 0.0f}, 0.0f, WHITE);
+       /*  switch (okno) {
+         case 'l':
+         }*/
         EndDrawing();
     }
 
+    UnloadTexture(tlo_epizod1);
     CloseWindow();
 
     return 0;
