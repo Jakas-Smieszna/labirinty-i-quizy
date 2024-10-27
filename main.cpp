36b4f0d0fd88c10457fd09357af21ae51cac626e
glamac <183738326+glamac@users.noreply.github.com>
2024-10-27 19:33:34 +0100

    a

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your firstAAA window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
