bd27c229edb06ddcb4abb29d15e0cf085a6432fd
Jarimorajro <jaromirjang@gmail.com>
2024-10-27 20:18:26 +0100

    Kot kot kot kot

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int main() {
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(RED);
+        DrawText("Congrats!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
