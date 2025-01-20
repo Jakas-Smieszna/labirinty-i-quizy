5d484c32ab1d74a3c262fb2a234dd964c60694cf
Miłosz Fido <miloszfid@gmail.com>
2025-01-20 21:31:52 +0100

    Wywalone polskie zmnaki

--- instrukcja.cpp
+++ instrukcja.cpp
@@ -9,7 +9,7 @@
 
 namespace instrukcja {
 	_Przycisk* Buttons[] = {
-		new PrzyciskTekst("Powróæ",{0, 1, 0.1, -1.3}, 30, [&] {stanGry = StanEkranu::MAIN_MENU; }),
+		new PrzyciskTekst("Powrot",{0, 1, 0.1, -1.3}, 30, [&] {stanGry = StanEkranu::MAIN_MENU; }),
 	};
 	void drawInstruction() {
 		ClearBackground(BLUE);
@@ -18,7 +18,7 @@ namespace instrukcja {
 		vector<string> lines = DivideText("Instrukcja.txt");
 		for (int i = 0; i < 14 && i<lines.size(); i++)
 		{
-			DrawText(lines[i].c_str(), 10, GetScreenHeight() * (height + 0.05*i), 20, BLACK);
+			DrawText(lines[i].c_str(), 10, GetScreenHeight() * (height + 0.05 * i), 20, BLACK);
 		}
 		for (auto& b : Buttons) {
 			b->draw();
--- ustawienia.cpp
+++ ustawienia.cpp
@@ -11,15 +11,15 @@ namespace ustawienia {
 #define LABIRYNTX 0.04
 #define QUESTIONX (LABIRYNTX+0.21)
 	_Przycisk* Buttons[] = {
-		new PrzyciskTekst("Powróć",{0, 1, 0.1, -1.3}, 30, [&] {stanGry = StanEkranu::MAIN_MENU;}),
+		new PrzyciskTekst("Powrot",{0, 1, 0.1, -1.3}, 30, [&] {stanGry = StanEkranu::MAIN_MENU;}),
 		// poziom trudności labiryntu
-		new RadioPrzyciskTekst("Początkujący", {LABIRYNTX, 0.3, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = '0'; }, [&] {return zmienne->trudnosc_labirynt == '0'; }),
-		new RadioPrzyciskTekst("Łatwy", {LABIRYNTX, 0.37, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = 'l'; }, [&] {return zmienne->trudnosc_labirynt == 'l'; }),
+		new RadioPrzyciskTekst("Poczatkujacy", {LABIRYNTX, 0.3, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = '0'; }, [&] {return zmienne->trudnosc_labirynt == '0'; }),
+		new RadioPrzyciskTekst("Latwy", {LABIRYNTX, 0.37, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = 'l'; }, [&] {return zmienne->trudnosc_labirynt == 'l'; }),
 		new RadioPrzyciskTekst("Normalny", {LABIRYNTX, 0.44, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = 'n'; }, [&] {return zmienne->trudnosc_labirynt == 'n'; }),
 		new RadioPrzyciskTekst("Trudny", {LABIRYNTX, 0.51, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = 't'; }, [&] {return zmienne->trudnosc_labirynt == 't'; }),
 		new RadioPrzyciskTekst("Mistrzowski", {LABIRYNTX, 0.58, 0, 0}, 26, [&] {zmienne->trudnosc_labirynt = 'm'; }, [&] {return zmienne->trudnosc_labirynt == 'm'; }),
 		// poziomy trudności pytań
-		new RadioPrzyciskTekst("Uczeń", {QUESTIONX, 0.3, 0, 0}, 26, [&] {zmienne->trudnosc_pytania = '2'; }, [&] {return zmienne->trudnosc_pytania == '2'; }),
+		new RadioPrzyciskTekst("Uczen", {QUESTIONX, 0.3, 0, 0}, 26, [&] {zmienne->trudnosc_pytania = '2'; }, [&] {return zmienne->trudnosc_pytania == '2'; }),
 		new RadioPrzyciskTekst("Odkrywca", {QUESTIONX, 0.37, 0, 0}, 26, [&] {zmienne->trudnosc_pytania = '6'; }, [&] {return zmienne->trudnosc_pytania == '6'; }),
 		new RadioPrzyciskTekst("Ekspert", {QUESTIONX, 0.44, 0, 0}, 26, [&] {zmienne->trudnosc_pytania = 's'; }, [&] {return zmienne->trudnosc_pytania == 's'; }),
 	};
@@ -28,9 +28,9 @@ namespace ustawienia {
 	//RadioPrzycisk hiVol("VOL>50", 0.55, 0.4, 30, BUTTON_NONE, [&] {Volume.change(75); }, [&] {return Volume.value >= 50; });
 	void initSettings() {
 		Volume.value = (zmienne->glosnosc)/100;
-		volstr = "Głośność: " + std::to_string(static_cast<int>(Volume.value*100)) + "%";
+		volstr = "Glosnosc: " + std::to_string(static_cast<int>(Volume.value*100)) + "%";
 		Volume.onChange = [&](float changed) {
-			volstr = "Głośność: " + std::to_string(static_cast<int>(changed*100)) + "%";
+			volstr = "Glosnosc: " + std::to_string(static_cast<int>(changed*100)) + "%";
 			zmienne->glosnosc = changed * 100;
 			};
 	}
@@ -45,9 +45,9 @@ namespace ustawienia {
 		}
 		DrawRectangleRec({ szer * 0.005f, wys * 0.005f, szer * 0.99f, wys * 0.99f }, Fade(EpisodeTheme.bgColor, 0.5));
 		helper::DrawTextCentered("Ustawienia", GetScreenWidth() / 2, GetScreenHeight() * 0.10, 30, WHITE);
-		DrawText("Poziomy Trudności", GetScreenWidth() * LABIRYNTX, GetScreenHeight() * 0.2, 30, WHITE);
+		DrawText("Poziomy Trudnosci", GetScreenWidth() * LABIRYNTX, GetScreenHeight() * 0.2, 30, WHITE);
 		DrawText("Labiryntu:", GetScreenWidth() * LABIRYNTX, GetScreenHeight() * 0.25, 26, WHITE);
-		DrawText("Pytań:", GetScreenWidth() * QUESTIONX, GetScreenHeight() * 0.25, 26, WHITE);
+		DrawText("Pytan:", GetScreenWidth() * QUESTIONX, GetScreenHeight() * 0.25, 26, WHITE);
 		DrawText(volstr.c_str(), GetScreenWidth() * 0.3, GetScreenHeight() * 0.65, 20, WHITE);
 		for (auto& b : Buttons) {
 			b->draw();
