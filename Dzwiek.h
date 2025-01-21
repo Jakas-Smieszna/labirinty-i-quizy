2aa005ac298ec1d5c78ff0e311a0c55bdfd24704
glamac <183738326+glamac@users.noreply.github.com>
2025-01-21 11:26:54 +0100

    idk rzeczy (#49)
    Co-authored-by: aleks <alexgrudniok@student.agh.edu.pl>

---
+++ Dzwiek.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "raylib.h"
+
+extern Sound sound;
+extern Music music;
+extern Music music2;
+
+void InitSounds(); // Funkcja inicjalizuj¹ca dŸwiêki
+void UnloadSounds(); // Funkcja zwalniaj¹ca pamiê
--- mainMenu.h
+++ mainMenu.h
@@ -1,6 +1,7 @@
 ﻿#pragma once 
 //#include "raylib.h"
 #include "Grafika.h"
+#include "Dzwiek.h"
 namespace menu {
 	void updateMainMenu();
 	void drawMainMenu();
